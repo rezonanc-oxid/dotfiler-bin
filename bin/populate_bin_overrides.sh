32a924019867ccfea1ce97b8f33f2a299ad8d89a
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-05-27 14:00:22 +0300

    Initial commit

---
+++ bin/populate_bin_overrides.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s $HOME/homebrew/Cellar/curl/7.48.0/bin/curl $HOME/bin/curl
+ln -s $HOME/homebrew/bin/git $HOME/bin/git
+ln -s $HOME/homebrew/bin/python $HOME/bin/python
+ln -s $HOME/homebrew/bin/ruby $HOME/bin/ruby
+ln -s $HOME/homebrew/bin/vim $HOME/bin/vim
+ln -s $HOME/homebrew/bin/vimdiff $HOME/bin/vimdiff
+ln -s $HOME/homebrew/bin/vimtutor $HOME/bin/vimtutor
+ln -s $HOME/homebrew/bin/zsh $HOME/bin/zsh
